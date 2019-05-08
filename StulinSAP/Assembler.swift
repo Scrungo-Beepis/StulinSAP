106c5ebffa0be6fc15707229a39aa12835cfd929
Scrungo-Beepis <m.lvovsky66@gmail.com>
2019-05-07 21:41:55 -0400

    vm running turing is done

--- StulinSAP/Assembler.swift
+++ StulinSAP/Assembler.swift
@@ -12,29 +12,84 @@ class Assembler {
     var symbolTable : [String:Int] = [:]
     let commands : [String:(bin: Int, argType: argType)] = [
         "halt":(0, argType.none),
+        "clrr":(1, argType.r),
+        "clrx":(2, argType.r),
+        "clrm":(3, argType.l),
+        "clrb":(4, argType.rr),
+        "movir":(5, argType.rr),
+        "movrr":(6, argType.rr),
+        "movrm":(7, argType.rl),
         "movmr":(8, argType.lr),
-        "outs":(55, argType.l),
-        "printi":(49, argType.r),
-        "outcr":(45, argType.r),
-        "cmprr":(34, argType.rr),
+        "movxr":(9, argType.rr),
+        "movar":(10, argType.lr),
+        "movb":(11, argType.rrr),
         "addir":(12, argType.rr),
-        "jmpne":(57, argType.l),
         "addrr":(13, argType.rr),
-        "movrr":(6, argType.rr)]
-    let directives = [
+        "addmr":(14, argType.lr),
+        "addxr":(15, argType.rr),
+        "subir":(16, argType.rr),
+        "subrr":(17, argType.rr),
+        "submr":(18, argType.lr),
+        "subxr":(19, argType.rr),
+        "mulir":(20, argType.rr),
+        "mulrr":(21, argType.rr),
+        "mulmr":(22, argType.lr),
+        "mulxr":(23, argType.rr),
+        "divir":(24, argType.rr),
+        "divrr":(25, argType.rr),
+        "divmr":(26, argType.lr),
+        "divxr":(27, argType.rr),
+        "jmp":(28, argType.l),
+        "sojz":(29, argType.rl),
+        "sojnz":(30, argType.rl),
+        "aojz":(31, argType.rl),
+        "aojnz":(32, argType.rl),
+        "cmpir":(33, argType.rr),
+        "cmprr":(34, argType.rr),
+        "cmpmr":(35, argType.lr),
+        "jmpn":(36, argType.l),
+        "jmpz":(37, argType.l),
+        "jmpp":(38, argType.l),
+        "jsr":(39, argType.l),
+        "ret":(40, argType.none),
+        "push":(41, argType.r),
+        "pop":(42, argType.r),
+        "stackc":(43, argType.r),
+        "outci":(44, argType.r),
+        "outcr":(45, argType.r),
+        "outcx":(46, argType.r),
+        "outcb":(47, argType.rr),
+        "readi":(48, argType.rr),
+        "printi":(49, argType.r),
+        "readc":(50, argType.r),
+        "readln":(51, argType.lr),
+        "brk":(52, argType.none),
+        "movrx":(53, argType.rr),
+        "movxx":(54, argType.rr),
+        "outs":(55, argType.l),
+        "nop":(56, argType.none),
+        "jmpne":(57, argType.l)]
+    /*let directives = [
         ".Integer",
-        ".String"]
+        ".String",
+        ".Tuple"]*/
     func assemble(_ assembly:String) throws -> (bin:[Int], lst:String, sym:String) {
         var start:Int? = nil
         var lst:[(lst:String, isLabel:Bool)] = []
         var result : [Int] = []
         let lines = assembly.components(separatedBy: "\n")
+        for n in 0..<lines.count {
+            if lines[n].contains(".Start") {
+                start = n
+                break
+            }
+        }
         symbolTable = [:]
         for lineNum in 0..<lines.count {
             lst.append(("\(result.count): ", false))
             var thisLine = lines[lineNum].components(separatedBy: ";").first!.components(separatedBy: " ").filter({$0 != ""})
             if thisLine.count > 0 {
-                if !commands.keys.contains(thisLine[0]) && !directives.contains(thisLine[0]) {
+                if !commands.keys.contains(thisLine[0]) && (directives(rawValue: thisLine[0]) == nil) {
                     let symbol = String(thisLine[0].dropLast())
                     guard !symbolTable.keys.contains(symbol) else {
                         throw CompilerError("Symbol \(symbol) already exists.", lineNum)
@@ -47,13 +102,16 @@ class Assembler {
                 if let command = commands[thisLine[0]] {
                     if start == nil {start = result.count}
                     result.append(command.bin)
+                    lst[lineNum].lst += "\(command.bin) "
                     do {
                         switch command.argType {
                         case .lr:
                             let l0 = try checkLineForL(thisLine, 1, lineNum)
                             result.append(l0)
+                            lst[lineNum].lst += "\(l0) "
                             let r1 = try checkLineForR(thisLine, 2, lineNum)
                             result.append(r1)
+                            lst[lineNum].lst += "\(r1) "
                             break
                         case .l:
                             let l0 = try checkLineForL(thisLine, 1, lineNum)
@@ -74,28 +132,89 @@ class Assembler {
                             lst[lineNum].lst += "\(r1) "
                             break
                         case .none: break
+                        case .rrr:
+                            let r0 = try checkLineForR(thisLine, 1, lineNum)
+                            result.append(r0)
+                            lst[lineNum].lst += "\(r0)"
+                            let r1 = try checkLineForR(thisLine, 2, lineNum)
+                            result.append(r1)
+                            lst[lineNum].lst += "\(r1)"
+                            let r2 = try checkLineForR(thisLine, 3, lineNum)
+                            result.append(r2)
+                            lst[lineNum].lst += "\(r2)"
+                            break
+                        case .rl:
+                            let r0 = try checkLineForR(thisLine, 1, lineNum)
+                            result.append(r0)
+                            lst[lineNum].lst += "\(r0)"
+                            let l1 = try checkLineForL(thisLine, 2, lineNum)
+                            result.append(l1)
+                            lst[lineNum].lst += "\(l1)"
+                            break
                         }
                     } catch {
                         throw error
                     }
                 } else {
-                    switch thisLine[0] {
-                    case directives[0]:
+                    switch directives(rawValue: thisLine[0]) {
+                    case .Integer?:
                         if let lInt = Int(thisLine[1].dropFirst()) {
                             lst[lineNum].lst += "\(lInt) "
                             result.append(lInt)
+                        } else {
+                            throw CompilerError("\(lines[lineNum]) cannot define an Integer.", lineNum)
                         }
                         break
-                    case directives[1]:
-                        var string = assembly.components(separatedBy: "\n")[lineNum].components(separatedBy: """
-"
-""")[1]
+                    case .String?:
+                        var string = assembly.components(separatedBy: "\n")[lineNum].components(separatedBy: "\"")[1]
                         result.append(string.count)
                         for char in string.unicodeScalars {
-                            lst[lineNum].lst += "\(char.value ) "
+                            lst[lineNum].lst += "\(char.value) "
                             result.append(Int(char.value))
                         }
                         break
+                    case .Start?:
+                        break
+                    case .Tuple?:
+                        let valsString = lines[lineNum].components(separatedBy: "/")
+                        guard valsString.count > 2 else {
+                            throw CompilerError("\(lines[lineNum]) does not contain the right number of args and / to define a Tuple.", lineNum)
+                        }
+                        let vals = valsString[1].components(separatedBy: " ")
+                        guard vals.count == 5 else {
+                            throw CompilerError("\(lines[lineNum]) does not contain the right number of args between / to define a Tuple.", lineNum)
+                        }
+                        if vals[0] == "l" {
+                            lst[lineNum].lst += "\(-1) "
+                            result.append(-1)
+                        } else if vals[0] == "r" {
+                            lst[lineNum].lst += "\(1)"
+                            result.append(1)
+                        } else {
+                            throw CompilerError("First value of Tuple must be l or r, not \(vals[0])", lineNum)
+                        }
+                        guard let cState = Int(vals[1]) else {
+                            throw CompilerError("Second value of Tuple must be an Int, not \(vals[1])", lineNum)
+                        }
+                        lst[lineNum].lst += "\(cState) "
+                        result.append(cState)
+                        guard let nState = Int(vals[2]) else {
+                            throw CompilerError("Third value of Tuple must be an Int, not \(vals[2])", lineNum)
+                        }
+                        lst[lineNum].lst += "\(nState) "
+                        result.append(nState)
+                        guard let cChar = vals[3].first else {
+                            throw CompilerError("Fourth value of Tuple must be a char, not \(vals[3])", lineNum)
+                        }
+                        lst[lineNum].lst += "\(cChar) "
+                        result.append(Int(cChar.unicodeScalars.first!.value))
+                        guard let nChar = vals[4].first else {
+                            throw CompilerError("Fifth value of Tuple must be a char, not \(vals[4])", lineNum)
+                        }
+                        lst[lineNum].lst += "\(nChar) "
+                        result.append(Int(nChar.unicodeScalars.first!.value))
+                        break
+                        
                     default: break
                     }
                 }
@@ -137,7 +256,15 @@ class Assembler {
         case l
         case r
         case rr
+        case rrr
+        case rl
         case none
+    }
+    enum directives: String {
+        case String = ".String"
+        case Integer = ".Integer"
+        case Tuple = ".Tuple"
+        case Start = ".Start"
     }
     func fitToLeft(_ length: Int, _ str: String) -> String{
         if str.count > length {
--- StulinSAP/VM.swift
+++ StulinSAP/VM.swift
@@ -15,27 +15,300 @@ class VM {
     var memory:[Int] = []
     var reachDist = 0
     var compReg = 0//0: less than; 1: greater than; 2: equals
+    var stack: [Int] = []
+    var stackLimit: Int = 1024
+    var instructions = ["halt": 0, "clrr": 1, "clrx": 2, "clrm": 3, "clrb": 4, "movir": 5, "movrr": 6, "movrm": 7, "movmr": 8, "movxr": 9, "movar": 10, "movb": 11, "addir": 12, "addrr": 13, "addmr": 13, "addxr": 15, "subir": 16, "subrr": 17, "submr": 18, "subxr": 19, "mulir": 20, "mulrr": 21, "mulmr": 22, "milxr": 23, "divir": 24, "divrr": 25, "divmr": 26, "divxr": 27, "jmp": 28, "sojz": 29, "sojnz": 30, "aojz": 31, "aojnz": 32, "cmpir": 33, "cmprr": 34, "cmpmr": 35, "jmpn": 36, "jmpz": 37, "jmpp": 38, "jsr": 39, "ret": 40, "push": 41, "pop": 42, "stackc": 43, "outci": 44, "outcr": 45, "outcx": 46, "outcb": 47, "readi": 48, "printi": 49, "readc": 50, "readln": 51, "brk": 52, "movrx": 53, "movxx": 54, "outs": 55, "nop": 56, "jmpne": 57]
     func runCommand(_ command:Int) {
         switch command {
-        case 0: haltVM()
-        case 8: movmr()
-        case 55: outs()
-        case 49: printi()
-        case 45: outcr()
-        case 34: cmprr()
-        case 12: addir()
-        case 57: jmpne()
-        case 13: addrr()
-        case 6: movrr()
+        case instructions["halt"]: haltVM()
+        case instructions["clrr"]: clrr()
+        case instructions["clrx"]: clrx()
+        case instructions["clrm"]: clrm()
+        case instructions["clrb"]: clrb()
+        case instructions["movir"]: movir()
+        case instructions["movrr"]: movrr()
+        case instructions["movrm"]: movrm()
+        case instructions["movmr"]: movmr()
+        case instructions["movxr"]: movxr()
+        case instructions["movar"]: movar()
+        case instructions["movb"]: movb()
+        case instructions["addir"]: addir()
+        case instructions["addrr"]: addrr()
+        case instructions["addmr"]: addmr()
+        case instructions["addxr"]: addxr()
+        case instructions["subir"]: subir()
+        case instructions["subrr"]: subrr()
+        case instructions["submr"]: submr()
+        case instructions["subxr"]: subxr()
+        case instructions["mulir"]: mulir()
+        case instructions["mulrr"]: mulrr()
+        case instructions["mulmr"]: mulmr()
+        case instructions["mulxr"]: mulxr()
+        case instructions["divir"]: divir()
+        case instructions["divrr"]: divrr()
+        case instructions["divmr"]: divmr()
+        case instructions["divxr"]: divxr()
+        case instructions["jmp"]: jmp()
+        case instructions["sojz"]: sojz()
+        case instructions["sojnz"]: sojnz()
+        case instructions["aojz"]: aojz()
+        case instructions["cmpir"]: cmpir()
+        case instructions["cmprr"]: cmprr()
+        case instructions["cmpmr"]: cmpmr()
+        case instructions["jmpn"]: jmpn()
+        case instructions["jmpz"]: jmpz()
+        case instructions["jmpp"]: jmpp()
+        case instructions["jsr"]: jsr()
+        case instructions["ret"]: ret()
+        case instructions["push"]: push()
+        case instructions["pop"]: pop()
+        case instructions["stackc"]: stackc()
+        case instructions["outci"]: outci()
+        case instructions["outcr"]: outcr()
+        case instructions["outcx"]: outcx()
+        case instructions["outcb"]: outcb()
+        case instructions["readi"]: readi()
+        case instructions["printi"]: printi()
+        case instructions["readc"]: readc()
+        case instructions["readln"]: readln()
+        case instructions["brk"]: brk()
+        case instructions["movrx"]: movrx()
+        case instructions["movxx"]: movxx()
+        case instructions["outs"]: outs()
+        case instructions["nop"]: nop()
+        case instructions["jmpne"]: jmpne()
         default: break
         }
     }
     func doNext() {
-        runCommand(memory[programCounter])
-        incrementCounter()
+            runCommand(memory[programCounter])
+            incrementCounter()
+    }
+    func clrr() {
+        registers[getPointAfterNum(1)] = 0
+    }
+    func clrx() {
+        memory[registers[getPointAfterNum(1)]] = 0
+    }
+    func clrm() {
+        memory[getPointAfterNum(1)] = 0
+    }
+    func clrb() {
+        memory[registers[getPointAfterNum(1)]..<(registers[getPointAfterNum(1)] + registers[getPointAfterNum(2)])] = ArraySlice.init(repeating: 0, count: registers[getPointAfterNum(2)])
+    }
+    func movir() {
+        registers[getPointAfterNum(2)] = getPointAfterNum(1)
+    }
+    func movrr() {
+        registers[getPointAfterNum(2)] = registers[getPointAfterNum(1)]
+    }
+    func movrm() {
+        memory[getPointAfterNum(2)] = registers[getPointAfterNum(1)]
     }
     func movmr() {
         registers[getPointAfterNum(2)] = memory[getPointAfterNum(1)]
+    }
+    func movxr() {
+        registers[getPointAfterNum(2)] = memory[registers[getPointAfterNum(1)]]
+    }
+    func movar() {
+        registers[getPointAfterNum(2)] = memory[getPointAfterNum(1)]
+    }
+    func movb() {
+        memory[registers[getPointAfterNum(2)]..<(registers[getPointAfterNum(2)] + registers[getPointAfterNum(3)])] = memory[registers[getPointAfterNum(1)]..<(registers[getPointAfterNum(1)] + registers[getPointAfterNum(3)])]
+    }
+    func addir() {
+        registers[getPointAfterNum(2)] += getPointAfterNum(1)
+    }
+    func addmr() {
+        registers[getPointAfterNum(2)] += memory[getPointAfterNum(1)]
+    }
+    func addxr() {
+        registers[getPointAfterNum(2)] += memory[registers[getPointAfterNum(1)]]
+    }
+    func subir() {
+        registers[getPointAfterNum(2)] -=  getPointAfterNum(1)
+    }
+    func subrr() {
+        registers[getPointAfterNum(2)] -= registers[getPointAfterNum(1)]
+    }
+    func submr() {
+        registers[getPointAfterNum(2)] -= memory[getPointAfterNum(2)]
+    }
+    func subxr() {
+        registers[getPointAfterNum(2)] -= memory[registers[getPointAfterNum(1)]]
+    }
+    func mulir() {
+        registers[getPointAfterNum(2)] *= getPointAfterNum(1)
+    }
+    func mulrr() {
+        registers[getPointAfterNum(2)] *= registers[getPointAfterNum(1)]
+    }
+    func mulmr() {
+        registers[getPointAfterNum(2)] *= memory[getPointAfterNum(1)]
+    }
+    func mulxr() {
+        registers[getPointAfterNum(2)] = memory[registers[getPointAfterNum(1)]]
+    }
+    func divir() {
+        registers[getPointAfterNum(2)] /= getPointAfterNum(1)
+    }
+    func divrr() {
+        registers[getPointAfterNum(2)] /= registers[getPointAfterNum(1)]
+    }
+    func divmr() {
+        registers[getPointAfterNum(2)] /= memory[getPointAfterNum(1)]
+    }
+    func divxr() {
+        registers[getPointAfterNum(2)] /= memory[registers[getPointAfterNum(1)]]
+    }
+    func jmp() {
+        jump(getPointAfterNum(1))
+    }
+    func sojz() {
+        registers[getPointAfterNum(1)] -= 1
+        let dest = getPointAfterNum(2)
+        if registers[getPointAfterNum(1)] == 0 {
+            jump(dest)
+        }
+    }
+    func sojnz() {
+        registers[getPointAfterNum(1)] -= 1
+        let dest = getPointAfterNum(2)
+        if registers[getPointAfterNum(1)] != 0 {
+            jump(dest)
+        }
+    }
+    func aojz() {
+        registers[getPointAfterNum(1)] += 1
+        let dest = getPointAfterNum(2)
+        if registers[getPointAfterNum(1)] == 0 {
+            jump(dest)
+        }
+    }
+    func aojnz() {
+        registers[getPointAfterNum(1)] += 1
+        let dest = getPointAfterNum(2)
+        if registers[getPointAfterNum(1)] != 0 {
+            jump(dest)
+        }
+    }
+    func cmpir() {
+        let num = getPointAfterNum(1)
+        let reg = registers[getPointAfterNum(2)]
+        if  num > reg {
+            compReg = 1
+        } else if num == reg {
+            compReg = 2
+        } else {
+            compReg = 0
+        }
+    }
+    func cmpmr() {
+        let lab = memory[getPointAfterNum(1)]
+        let reg = registers[getPointAfterNum(2)]
+        if  lab > reg {
+            compReg = 1
+        } else if lab == reg {
+            compReg = 2
+        } else {
+            compReg = 0
+        }
+    }
+    func jmpn() {
+        let dest = getPointAfterNum(1)
+        if compReg == 0 {
+            jump(dest)
+        }
+    }
+    func jmpz() {
+        let dest = getPointAfterNum(1)
+        if compReg == 2 {
+            jump(dest)
+        }
+    }
+    func jmpp() {
+        let dest = getPointAfterNum(1)
+        if compReg == 1 {
+            jump(dest)
+        }
+    }
+    func jsr() {
+        for r in [9, 8, 7, 6, 5] {
+            stack.append(registers[r])
+        }
+        stack.append(programCounter + 2)
+        jump(getPointAfterNum(1))
+    }
+    func ret() {
+        jump(stack.popLast()!)
+        for r in 5...9 {
+            registers[r] = stack.popLast()!
+        }
+    }
+    func push() {
+        stack.append(registers[getPointAfterNum(1)])
+    }
+    func pop() {
+        registers[getPointAfterNum(1)] = stack.popLast()!
+    }
+    func stackc() {
+        if stackLimit == stack.count {
+            registers[getPointAfterNum(1)] = 1
+        } else if stack.count == 0  {
+            registers[getPointAfterNum(1)] = 2
+        } else {
+            registers[getPointAfterNum(1)] = 0
+        }
+    }
+    func outci() {
+        print(UnicodeScalar(getPointAfterNum(1))!, terminator: "")
+    }
+    func outcx() {
+        print(UnicodeScalar(memory[registers[getPointAfterNum(1)]])!, terminator: "")
+    }
+    func outcb() {
+        for n in registers[getPointAfterNum(1)]..<(registers[getPointAfterNum(1)]+registers[getPointAfterNum(2)]) {
+            print(UnicodeScalar(memory[n])!, terminator: "")
+        }
+    }
+    func readi() {
+        let errReg = getPointAfterNum(2)
+        let targetReg = getPointAfterNum(1)
+        guard let i = Int(readLine() ?? "") else {
+            registers[errReg] = 1
+            return
+        }
+        registers[targetReg] = i
+        registers[errReg] = 0
+    }
+    func readc() {
+        guard let c = readLine()?.first else {
+            return
+        }
+        registers[getPointAfterNum(1)] = Int(UnicodeScalar(String(c))!.value)
+    }
+    func readln() {
+        guard let line = readLine() else {
+            return
+        }
+        let start = getPointAfterNum(1)
+        registers[getPointAfterNum(2)] = line.count
+        for n in 0..<line.count {
+            memory[start + n] = Int(UnicodeScalar(String(line[line.index(line.startIndex, offsetBy: n)]))!.value)
+        }
+    }
+    func brk() {
+        
+    }
+    func movrx() {
+        memory[registers[getPointAfterNum(2)]] = registers[getPointAfterNum(1)]
+    }
+    func movxx() {
+        memory[registers[getPointAfterNum(2)]] = memory[registers[getPointAfterNum(1)]]
+    }
+    func nop() {
+        return
     }
     func outs() {
         print(getString(getPointAfterNum(1)), terminator: "")
@@ -44,7 +317,7 @@ class VM {
         print(registers[getPointAfterNum(1)], terminator: "")
     }
     func outcr() {
-        print(String(UnicodeScalar(registers[getPointAfterNum(1)])!), terminator: "")
+        print(Character(UnicodeScalar(getPointAfterNum(1))!), terminator: "")
     }
     func cmprr() {
         let reg0 = registers[getPointAfterNum(1)]
@@ -57,9 +330,6 @@ class VM {
             compReg = 0
         }
     }
-    func addir() {
-        registers[getPointAfterNum(2)] += getPointAfterNum(1)
-    }
     func jmpne() {
         let dest = getPointAfterNum(1)
         if compReg != 2 {
@@ -67,7 +337,7 @@ class VM {
         }
     }
     func jump(_ destination: Int) {
-        programCounter = destination
+        programCounter = destination - 1
         reachDist = 0
     }
     func addrr() {
@@ -75,9 +345,6 @@ class VM {
     }
     func getString(_ memLoc: Int) -> String {
         return memory[memLoc+1...memLoc+memory[memLoc]].reduce("", {$0 + String(UnicodeScalar($1)!)})
-    }
-    func movrr() {
-        registers[getPointAfterNum(2)] = registers[getPointAfterNum(1)]
     }
     func incrementCounter() {
         incrementCounter(reachDist + 1)
@@ -100,7 +367,11 @@ class VM {
         let iL = memory[0]
         memory.remove(at: 0)
         while (isRunning) {
-            doNext()
+            if (programCounter < memory.count) {
+                doNext()
+            } else {
+                isRunning = false
+            }
         }
         memory.insert(iPC, at: 0)
         memory.insert(iL, at: 0)
